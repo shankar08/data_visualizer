12e6e105e8e646ae58c97feeb2dd3385386ce23b
Shankar Prajapati <shanky@MacBook-Pro.local>
2026-01-25 21:12:32 -0500

    changed file structures

--- streamlit.py
+++ streamlit.py
@@ -7,7 +7,7 @@ SRC = ROOT / "src"
 if str(SRC) not in sys.path:
     sys.path.insert(0, str(SRC))
 
-from frontend.app import main
+from src.app import main
 
 if __name__ == "__main__":
     main()
